b73d4a1803ce8569db1b534208248ed00aab5435
Marcius Bezerra <marciusbezerra@outlook.com>
2017-08-18 02:06:28 -0300

    Modificações adicionais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { DataserviceService } from './dataservice.service';
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Pais } from './pais';
+import { Cidade } from './cidade';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-lista-paises',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [DataserviceService]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Combo Cascade';
+  paisSelecionado: Pais = new Pais(0, 'Brasil');
+  paises: Pais[];
+  cidades: Cidade[];
+
+  constructor(private _dataService: DataserviceService) {
+    this.paises = this._dataService.getPaises();
+  }
+
+  onSelect(id: string) {
+    this.cidades = this._dataService.getCidades().filter((c) => c.paisid === parseInt(id, null));
+  }
 }
